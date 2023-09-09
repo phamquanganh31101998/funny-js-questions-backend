1e0cc99829d1479f3cf09d2e82b629de14b6e455
Cheems <quanganhpham31101998@gmail.com>
2023-09-09 23:06:15 +0700

    Custom Provider

---
+++ src/cats/__mocks__/cats.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { Cat } from '../interfaces/cat.interface';
+
+@Injectable()
+export class CatsService {
+  private readonly cats: Cat[] = [];
+
+  createCat() {
+    return {};
+  }
+
+  findAll(): Cat[] {
+    return [];
+  }
+}
--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
+import { CatsService as MockCatsService } from './__mocks__/cats.service';
 
 @Module({
   controllers: [CatsController],
-  providers: [CatsService],
+  providers: [{ provide: CatsService, useClass: MockCatsService }],
   exports: [CatsService],
 })
 export class CatsModule {}
