68d53cda4406821184db794a97680d2e35c0b44b
Cheems <quanganhpham31101998@gmail.com>
2023-09-17 21:19:14 +0700

    Use native ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,28 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
+// import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { APP_FILTER } from '@nestjs/core';
 import { HttpExceptionFilter } from './common/filter/http-exception.filter';
-import { ConfigModule } from './config/config.module';
+// import { ConfigModule } from './config/config.module';
 import { QuestionsModule } from './questions/questions.module';
 import { StorageModule } from './storage/storage.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    CatsModule,
-    ConfigModule.forRoot({ folderPath: './config' }),
+    ConfigModule.forRoot({
+      envFilePath: ['.env', '.env.dev.example'],
+      cache: true,
+      isGlobal: true,
+    }),
     QuestionsModule,
     StorageModule,
+
+    // For learning purpose only
+    // CatsModule,
+    // ConfigModule.forRoot({ folderPath: './config' }),
   ],
   controllers: [AppController],
   providers: [
--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -1,11 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { IQuestion } from './interfaces/question.interface';
+import { EnvKey } from 'common/constants/env-key.constant';
 
 @Injectable()
 export class QuestionsService {
   private questionList: IQuestion[] = [];
+  private logger = new Logger(QuestionsService.name);
+
+  constructor(private configService: ConfigService) {}
 
   async getAllQuestions(): Promise<IQuestion[]> {
+    this.logger.verbose(this.configService.get<string>(EnvKey.DB_NAME));
     return this.questionList;
   }
 }
