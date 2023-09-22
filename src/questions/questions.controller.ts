f40ab61fc4915c1632b3066ea5d10a6931788432
Cheems <quanganhpham31101998@gmail.com>
2023-09-22 10:20:15 +0700

    22-09-2023: add API create question, create answer for question

--- src/questions/questions.controller.ts
+++ src/questions/questions.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get, UseInterceptors } from '@nestjs/common';
-import { IQuestion } from './interfaces/question.interface';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseInterceptors,
+} from '@nestjs/common';
 import { QuestionsService } from './questions.service';
 import { LoggingInterceptor } from 'common/interceptor/logging.interceptor';
 import { TransformInterceptor } from 'common/interceptor/transform.interceptor';
 import { Question } from '../storage/entities/Question.entity';
+import { ValidationPipe } from '../common/pipe/validation.pipe';
+import { CreateQuestionDto } from './dtos/create-question.dto';
+import { CreateAnswerForQuestionDto } from './dtos/create-answer-for-question.dto';
 
 @Controller('questions')
 @UseInterceptors(LoggingInterceptor, TransformInterceptor)
@@ -13,5 +23,24 @@ export class QuestionsController {
   @Get()
   async getAllQuestions(): Promise<Question[]> {
     return this.questionsService.getAllQuestions();
+  }
+
+  @Post()
+  async createQuestion(
+    @Body(new ValidationPipe()) createQuestionDto: CreateQuestionDto,
+  ): Promise<Question> {
+    return this.questionsService.createQuestion(createQuestionDto);
+  }
+
+  @Post('/:questionId/answer')
+  async createAnswerForQuestion(
+    @Param('questionId', ParseIntPipe) questionId: number,
+    @Body(new ValidationPipe())
+    createAnswerForQuestionDto: CreateAnswerForQuestionDto,
+  ): Promise<Question> {
+    return this.questionsService.createAnswerForQuestion(
+      questionId,
+      createAnswerForQuestionDto,
+    );
   }
 }
--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -1,10 +1,12 @@
 import { Repository } from 'typeorm';
-import { Injectable, Logger } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
-import { IQuestion } from './interfaces/question.interface';
 import { EnvKey } from 'common/constants/env-key.constant';
 import { Question } from 'storage/entities/Question.entity';
+import { CreateQuestionDto } from './dtos/create-question.dto';
+import { CreateAnswerForQuestionDto } from './dtos/create-answer-for-question.dto';
+import { Answer } from '../storage/entities/Answer.entity';
 
 @Injectable()
 export class QuestionsService {
@@ -18,6 +20,45 @@ export class QuestionsService {
 
   async getAllQuestions(): Promise<Question[]> {
     this.logger.verbose(this.configService.get<string>(EnvKey.DB_NAME));
-    return this.questionsRepository.find();
+    return this.questionsRepository.find({
+      relations: {
+        answers: true,
+      },
+    });
+  }
+
+  async createQuestion(
+    createQuestionDto: CreateQuestionDto,
+  ): Promise<Question> {
+    return this.questionsRepository.save(createQuestionDto);
+  }
+
+  async createAnswerForQuestion(
+    questionId: number,
+    createAnswerForQuestionDto: CreateAnswerForQuestionDto,
+  ): Promise<Question> {
+    const question = await this.questionsRepository.findOne({
+      where: {
+        id: questionId,
+      },
+      relations: {
+        answers: true,
+      },
+    });
+
+    if (!question) {
+      throw new HttpException(
+        `Cannot find question with id = ${questionId}`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    const newAnswer = new Answer({ text: createAnswerForQuestionDto.text });
+    const newAnswers = [...question.answers, newAnswer];
+
+    return await this.questionsRepository.save({
+      ...question,
+      answers: newAnswers,
+    });
   }
 }
